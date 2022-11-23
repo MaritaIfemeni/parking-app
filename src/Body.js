cce9eea9f58f7dda56d7ba40a3e82f529582a38b
MaritaIfemeni <marita.ifemeni@student.hamk.fi>
2022-11-23 21:26:44 +0200

    button add

--- src/Body.js
+++ src/Body.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Body = (props) => {
+  const [freePark, setFreePark] = useState("Muuttuuko?");
+  const [freePark2, setFreePark2] = useState("Muutt?");
+  const [oldState, setOldState] = useState(null);
+
+  const listOfFreeSpots = [
+    "Aurinkokiven koulu",
+    "Keravan asema",
+    "Jumbon Parkkialue",
+    "Pielaveden K-Marketti",
+  ];
+
+  const getRandom = (list) => {
+    return Math.floor(Math.random() * list.length);
+  };
+// redo this later
+  const handleClick = () => {
+    let choiceOne = getRandom(listOfFreeSpots);
+    setFreePark(listOfFreeSpots[choiceOne]);
+    if (freePark == oldState){
+      choiceOne = getRandom(listOfFreeSpots);
+      setFreePark(listOfFreeSpots[choiceOne]);
+    }
+    let choiceTwo = getRandom(listOfFreeSpots);
+    if (choiceTwo == choiceOne){
+      if (choiceOne == 4){
+        choiceTwo -= 1
+      } else {
+        choiceTwo += 1 
+      }
+    }
+    setFreePark2(listOfFreeSpots[choiceTwo]);
+    setOldState(freePark)
+  };
+
   return (
     <div>
-      <p id="test"> Valitse parkkialue </p>{" "}
+      <p> Vapaita parkkipaikkoja seuraavissä kohteissa: </p>
+      {freePark}
+      {freePark2}
+      <button className="button" onClick={handleClick}>
+        Paina
+      </button>
     </div>
   );
 };
